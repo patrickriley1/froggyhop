84f7fd53ee893561ed976ab1e4b62eb4679018cd
Patrick Riley <righthandriley32@gmail.com>
2025-07-15 10:04:06 -0500

    level 1 fixes

--- froggy/src/Levels/Level1.jsx
+++ froggy/src/Levels/Level1.jsx
@@ -27,16 +27,16 @@ function Level1() {
 
 
     return (
-        <body className="level1-body">
+        <div className="level1-body">
             <audio autoPlay loop>
                 <source src="/Level1music.mp3" type="audio/mpeg" />
                 Your browser does not support the audio element.
             </audio>
             <div className="level1-container">
                 <h1>Level 1</h1>
-                <button onClick={() => navigate(-1)}>Back</button>
+                <button className="menu-button" onClick={() => navigate(-1)}>Back</button>
             </div>
-        </body>
+        </div>
 
     );
 }

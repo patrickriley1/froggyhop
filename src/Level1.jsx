21bf6f56f15f3d9a543f9339bbf40569bc0850c8
Patrick Riley <righthandriley32@gmail.com>
2025-08-08 16:59:50 -0400

    removed backgroundx

--- src/Level1.jsx
+++ src/Level1.jsx
@@ -9,7 +9,7 @@ function Level1() {
     const GAME_WIDTH = '100vw';
     const GAME_HEIGHT = '100vh';
     const GROUND_Y = 740;
-    const GRAVITY = .4;
+    const GRAVITY = 1;
     const JUMP_VELOCITY = -15;
 
     // State
@@ -47,14 +47,18 @@ const musicRef = useRef(null);
 
 useEffect(() => {
     if (!gameStarted) return;
-    let raf;
-    const moveBg = () => {
-      setBackgroundX(x => (x - 2)); // Move background left
-      raf = requestAnimationFrame(moveBg);
-    };
-    moveBg();
-    return () => cancelAnimationFrame(raf);
-  } , [gameStarted]);
+  let x = 0;
+  function move() {
+    x -= 4;
+    // here we do a direct DOM write instead of setState
+    if (bgRef.current) {
+      bgRef.current.style.backgroundPositionX = x + "px";
+    }
+    requestAnimationFrame(move);
+  }
+  const id = requestAnimationFrame(move);
+  return () => cancelAnimationFrame(id);
+}, [gameStarted]);
 
   // Main game loop
   useEffect(() => {
@@ -90,9 +94,12 @@ useEffect(() => {
   }, []);
 
   return (
-    <div className="level1-body"
-      style={{ backgroundPositionX: `${backgroundX}px` }}
-      tabIndex={0}>
+    <div
+      ref={bgRef}
+      className="level1-body"
+      tabIndex={0}
+      onClick={handleJump}
+    >
       <audio ref={musicRef} autoPlay loop>
         <source src="/Level1music.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
@@ -101,31 +108,31 @@ useEffect(() => {
       <button className="menu-button" onClick={() => navigate(-1)}>Back</button>
       {/* Game area */}
       <div
-                style={{
-                    position: 'relative',
-                    width: GAME_WIDTH,
-                    height: GAME_HEIGHT,
-                    background: 'transparent',
-                }}
-                onClick={handleJump} // click to jump (for mobile)
-            >
-                {/* Frog */}
-                <img
-                    src="frog.png"
-                    alt="Frog"
-                    style={{
-                        position: 'absolute',
-                        left: '50%',
-                        top: frogY,
-                        transform: 'translateX(-50%)',
-                        width: 100,
-                        height: 100
-                    }}
-                />
-                {!gameStarted && (<p style={{ color: 'white', textAlign: 'center', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>Press <b>Space</b> or click the box to jump!</p>)}
-            </div>
-        </div>
-    );
+        style={{
+          position: 'relative',
+          width: GAME_WIDTH,
+          height: GAME_HEIGHT,
+          background: 'transparent',
+        }}
+        onClick={handleJump} // click to jump (for mobile)
+      >
+        {/* Frog */}
+        <img
+          src="frog.png"
+          alt="Frog"
+          style={{
+            position: 'absolute',
+            left: '50%',
+            top: frogY,
+            transform: 'translateX(-50%)',
+            width: 100,
+            height: 100
+          }}
+        />
+        {!gameStarted && (<p style={{ color: 'white', textAlign: 'center', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>Press <b>Space</b> or click the box to jump!</p>)}
+      </div>
+    </div>
+  );
 }
 
 export default Level1;
